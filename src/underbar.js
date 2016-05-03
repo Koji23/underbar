56bd8a169dded228fed3348b1af87ddc9f561468
Jordan <jordanchong23@gmail.com>
2016-05-02 22:38:42 -0700

    "Complete _.sortBy"

--- src/underbar.js
+++ src/underbar.js
@@ -392,6 +392,13 @@
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    return collection.sort(function(a,b){
+      if(typeof iterator === 'function'){
+        return iterator(a) - iterator(b);
+      } else if(typeof iterator === 'string'){
+        return a[iterator] - b[iterator];
+      }
+    });
   };
 
   // Zip together two or more arrays with elements of the same index
